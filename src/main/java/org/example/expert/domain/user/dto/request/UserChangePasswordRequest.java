f120ad84c9ca71848ce3ff1903389446b2b99351
wonchaebin <usan119@naver.com>
2025-02-27 18:04:14 +0900

    레벨 1-3: Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +15,7 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String oldPassword;
     @NotBlank
+    @Size(min = 8)
+    @Pattern(regexp = "^(?=.*\\\\d)(?=.*[A-Z]).{8,}$", message = "새 비밀번호는 8자 이상이어야 하고 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
